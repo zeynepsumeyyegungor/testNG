fa556128776af09f29e11e3da633a0c294f024dc
Zeynep Sumeyye Gungor <gungorsumeyye3520@gmail.com>
2022-10-22 11:02:30 +0300

    22/10/22

---
+++ src/test/java/ZEYNEB_LOCATELER.java
@@ -0,0 +1,49 @@
+public class ZEYNEB_LOCATELER {
+
+    //  //Zeynep locate'ler
+    //    @FindBy (xpath = "//*[text()='Sign In']")
+    //    public WebElement signIn;
+    //    @FindBy (xpath = "//input[@id='username']")
+    //    public WebElement emailTextBox;
+    //    @FindBy (xpath = "//input[@id='password']")
+    //    public WebElement passwordTextBox;
+    //    @FindBy(xpath = "(//button[@type='submit'])[1]")
+    //    public WebElement signIn2;
+    //    @FindBy (xpath = "//*[text()='Sign Out']")
+    //    public WebElement singOut;
+    //    @FindBy (xpath = "//*[text()='Store Manager']")
+    //    public WebElement storeManager;
+    //    @FindBy (xpath = "//span[normalize-space()='Customers']")
+    //    public WebElement customers;
+    //
+    //    @FindBy (xpath = "//*[@class='sorting_asc']")
+    //    public WebElement customerName;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[1]")
+    //    public WebElement customerUsername;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[2]")
+    //    public WebElement customerEmail;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[3]")
+    //    public WebElement customerLocation;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[4]")
+    //    public WebElement customerOrders;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[5]")
+    //    public WebElement customerMoneySpent;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[6]")
+    //    public WebElement customerLastOrder;
+    //
+    //    @FindBy (xpath = "(//*[@class='sorting_disabled'])[7]")
+    //    public WebElement customerActions;
+    //
+    //    @FindBy (xpath = "//*[@class='dt-button buttons-pdf buttons-html5']")
+    //    public WebElement customerPdf;
+    //
+    //
+    //    @FindBy (xpath = "//*[@class='dt-button buttons-print']")
+    //    public WebElement customerPrint;
+}
